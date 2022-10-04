6fee02732fdb1d075a553c001ee2c77f5742fe82
HARSHA GUPTA <guptaharsha.harsha@gmail.com>
2022-10-04 20:35:26 +0530

    Activating the button and adding some transitions

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const dropzone = document.querySelector(".box2");
+const button = document.querySelector("#button");
+const fileinput = document.querySelector("#fileinput");
+dropzone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    if (!dropzone.classList.contains("dragged"))
+        dropzone.classList.add("dragged");
+});
+
+dropzone.addEventListener("dragleave", () => {
+    dropzone.classList.remove("dragged");
+});
+
+dropzone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropzone.classList.remove("dragged");
+    const files = e.dataTransfer.files;
+    console.log(files);
+    if (files.length) {
+        fileinput.file = files;
+    }
+});
+
+button.addEventListener("click", () => {
+    fileinput.click();
+});
